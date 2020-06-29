ae447b2cd381a0a69b1a258004ec15a71dab5970
Achraf Chennan <Achraf.Chennan@ordina.nl>
2020-06-29 10:18:32 +0200

    Add EF core and DpInjection

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
+using System.IO.Enumeration;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleTodoList.Infrastructure.DI;
 
 namespace SimpleTodoList
 {
@@ -13,5 +18,29 @@ namespace SimpleTodoList
     /// </summary>
     public partial class App : Application
     {
+        public IServiceProvider ServiceProvider {get;private  set;}
+        public IConfiguration Configuration {get;private  set;}
+
+        protected  override void  OnStartup(StartupEventArgs eventArgs)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json",false,true);
+
+            Configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection serviceCollection)
+        {
+            serviceCollection.RegisterInfrastructureDependencies(Configuration);
+            serviceCollection.AddTransient(typeof(MainWindow));
+        }
     }
 }
